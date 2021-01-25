b4269bd7249ea11b0a9547be16c1f5a67b1e81e6
M. Serdar Karaman <m.serdar.karaman@gmail.com>
2021-01-25 16:39:42 +0300

    +Şablon tartışmaları https://tr.wikipedia.org/w/index.php?title=Kullan%C4%B1c%C4%B1_mesaj:Evrifaessa&type=revision&diff=24701829&oldid=24701787&diffmode=source

--- st.py
+++ st.py
@@ -13,9 +13,10 @@ import mavri
 wiki = 'tr.wikipedia'
 username = 'Arşivleyici'
 xx = mavri.login(wiki, username)
-title = 'Vikipedi:Silinmeye aday sayfalar'
+title = 'Vikipedi:Şablon tartışmaları'
 version = 'V3.0g'
-summary_ek = " (" + username + ", " + version + " running on " + platform.system() + "), ([[Kullanıcı mesaj:Evrifaessa|hata bildir]])"
+summary_ek = " (" + username + ", " + version + " running on " + \
+    platform.system() + "), ([[Kullanıcı mesaj:Evrifaessa|hata bildir]])"
 ignore_list = []
 mpa = dict.fromkeys(range(32))
 utc = pytz.timezone('UTC')
@@ -40,18 +41,20 @@ while 1:
     now = datetime.now(ist)
     content = mavri.content_of_page(wiki, title)
 
-    regex = r"(?<=Silinmeye aday sayfalar/)[^}]*"
+    regex = r"(?<=Şablon tartışmaları/)[^}]*"
 
     if content != '{{/Başlık}}' and content != '{{/Başlık}}\n== Tartışmalar ==':
         pages = re.findall(regex.decode('UTF-8'), content)
         for page in pages:
             try:
-                pageContent = mavri.content_of_page(wiki, "Vikipedi:Silinmeye_aday_sayfalar/" + page.decode('UTF-8'))
+                pageContent = mavri.content_of_page(wiki, title +"/" + page.decode('UTF-8'))
                 timestampMonth = monthList[now.month-1]
                 preTimestampMonth = monthList[now.month-2]
                 timestampYear = now.year
-                archivePage = "Vikipedi:Silinmeye_aday_sayfalar/Kayıt/" + str(timestampYear) + "_" + str(timestampMonth)
-                preArchivePage = "Vikipedi:Silinmeye_aday_sayfalar/Kayıt/" + str(timestampYear) + "_" + str(preTimestampMonth)
+                archivePage = title +"/Kayıt/" + \
+                    str(timestampYear) + "_" + str(timestampMonth)
+                preArchivePage = title +"/Kayıt/" + \
+                    str(timestampYear) + "_" + str(preTimestampMonth)
                 contentLow = pageContent.lower()
                 resolved = '{{sas son}}' in contentLow
                 pinned = "{{mesaj sabitle" in contentLow or "{{pin message" in contentLow or "{{mesaj_sabitle" in contentLow or "{{pin_message" in contentLow
@@ -72,34 +75,45 @@ while 1:
                 content2 = content2.replace("Aralık", "December")
 
                 regex = r"\d{2}\.\d{2}\,\s\d{1,2}\s\w+\s\d{4}\s\(UTC\)"
-                matches = re.finditer(regex, content2.decode('UTF-8'), re.MULTILINE)
+                matches = re.finditer(
+                    regex, content2.decode('UTF-8'), re.MULTILINE)
 
                 signatureTimes = []
 
                 for matchNum, match in enumerate(matches, start=1):
-                    date_time_obj = utc.localize(datetime.strptime(str(match.group()), '%H.%M, %d %B %Y (%Z)'))
+                    date_time_obj = utc.localize(datetime.strptime(
+                        str(match.group()), '%H.%M, %d %B %Y (%Z)'))
                     signatureTimes.append(date_time_obj)
 
                 youngest = max(dt for dt in signatureTimes if dt < now)
                 youngestDiff = now - youngest
 
                 archiveContent = mavri.content_of_page(wiki, archivePage.decode('UTF-8'))
-                preArchiveContent = mavri.content_of_page(wiki, preArchivePage.decode('UTF-8'))
-                hasBeenPreArchived = '{{Vikipedi:Silinmeye aday sayfalar/' + page + '}}' in preArchiveContent
-                hasBeenArchived = '{{Vikipedi:Silinmeye aday sayfalar/' + page + '}}' in archiveContent
+                preArchiveContent = mavri.content_of_page(
+                    wiki, preArchivePage.decode('UTF-8'))
+                hasBeenPreArchived = '{{'+title +"/" + \
+                    page + '}}' in preArchiveContent
+                hasBeenArchived = '{{title +"/" + \
+                    page + '}}' in archiveContent
 
                 if hasBeenArchived == False and hasBeenPreArchived == False:
-                    append = '\n' + '{{Vikipedi:Silinmeye aday sayfalar/' + page + '}}'
-                    archiveSummary = 'Arşiv sayfalarında bulunmayan [[Vikipedi:Silinmeye_aday_sayfalar/' + page + '|SAS alt sayfası]] arşivlere ekleniyor - ' + summary_ek
-                    mavri.appendtext_on_page(wiki, archivePage.decode('UTF-8'), append, archiveSummary, xx)
+                    append = '\n' + \
+                        '{{'+title +"/" + page + '}}'
+                    archiveSummary = 'Arşiv sayfalarında bulunmayan [['+title +"/" + \
+                        page + '|SAS alt sayfası]] arşivlere ekleniyor - ' + summary_ek
+                    mavri.appendtext_on_page(wiki, archivePage.decode(
+                        'UTF-8'), append, archiveSummary, xx)
                     print(page + ' arşiv sayfasına eklendi.')
 
                 if pinned == False:
                     if resolved and youngestDiff.total_seconds() >= 3600:
-                        summary = '[[Vikipedi:Silinmeye_aday_sayfalar/' + page + '|Sonuçlandırılan SAS]] arşivleniyor - ' + summary_ek
-                        print(page + " SAS sayfasından kaldırılıyor.")
-                        newContent = content.replace("{{Vikipedi:Silinmeye aday sayfalar/" + page + "}}", "")
-                        newContent = re.sub(r"^[ \t]*$\r?\n", "", newContent, flags=re.MULTILINE)
+                        summary = '[['+title +'/' + page + \
+                            '|Sonuçlandırılan ST]] arşivleniyor - ' + summary_ek
+                        print(page + " ST sayfasından kaldırılıyor.")
+                        newContent = content.replace(
+                            "{{"+title +'/' + page + "}}", "")
+                        newContent = re.sub(
+                            r"^[ \t]*$\r?\n", "", newContent, flags=re.MULTILINE)
                         mavri.change_page(wiki, title, newContent, summary, xx)
                 else:
                     print(page + ' sabitlenmiş.')
